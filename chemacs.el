7538c39382fd53e775687b67cbb490e4c14547d8
Richard Kim <emacs18@gmail.com>
2020-12-20 22:35:40 -0800

    use ~/.emacs.d/.emacs-profiles.el if it exist

--- chemacs.el
+++ chemacs.el
@@ -25,8 +25,13 @@
 
 (defvar chemacs-version "2.0")
 (defvar config-home (or (getenv "XDG_CONFIG_HOME") "~/.config"))
-(defvar chemacs-profiles-paths (list "~/.emacs-profiles.el" (format "%s/%s" config-home "chemacs/profiles.el" )))
-(defvar chemacs-default-profile-paths (list "~/.emacs-profile" (format "%s/%s" config-home "chemacs/profile")))
+(defvar chemacs-profiles-paths
+  (list "~/.emacs.d/.emacs-profiles.el"
+        "~/.emacs-profiles.el"
+        (format "%s/%s" config-home "chemacs/profiles.el" )))
+(defvar chemacs-default-profile-paths
+  (list "~/.emacs-profile"
+        (format "%s/%s" config-home "chemacs/profile")))
 
 (defvar chemacs-profiles-path (or (car (seq-filter 'file-exists-p chemacs-profiles-paths)) (car chemacs-profiles-paths)))
 (defvar chemacs-default-profile-path (or (car (seq-filter 'file-exists-p chemacs-default-profile-paths)) (car chemacs-default-profile-paths)))
